dcf45149920d6b995e4b7c1566288b78535f566c
Michael Waldo <mbwcogic@yahoo.com>
2019-11-10 14:22:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,8 +66,10 @@ def second_supply_for_fourth_of_july(supplies_hash)
   supplies_hash[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holiday(supplies_hash, supply)
-  supplies_hash[:winter][:new_years] << supply
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
 end
 
 def add_supply_to_memorial_day(supplies_hash, supply)
